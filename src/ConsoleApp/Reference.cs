15ed5a4b51737f75946abfba3a362cfa11543ded
Camilo Bernal <camilo.bernal@banlinea.com>
2016-12-21 08:17:43 -0500

    Add AddReference method

---
+++ src/ConsoleApp/Reference.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    public class Reference
+    {
+        public string Xpath { get; set; }
+        public List<string> Transforms { get;  } = new List<string>();
+        public string DigestAlgorithm { get; set; }
+        public string Uri { get; set; }
+        public string DigestValue { get; set; }
+        public string InclusiveNamespacesPrefixList { get; set; }
+        public bool IsEmptyUri { get; set; }
+    }
+}
--- src/ConsoleApp/SignedXml.cs
+++ src/ConsoleApp/SignedXml.cs
@@ -1,37 +1,13 @@
-
 using System.Collections.Generic;
 
 namespace ConsoleApp
 {
     public class SignedXml
     {
-
-        /*
-           this.options = options || {};
-  this.idMode = idMode
-  this.references = []
-  this.id = 0
-  this.signingKey = null
-  this.signatureAlgorithm = this.options.signatureAlgorithm || "http://www.w3.org/2000/09/xmldsig#rsa-sha1";
-  this.keyInfoProvider = null
-  this.canonicalizationAlgorithm = "http://www.w3.org/2001/10/xml-exc-c14n#"
-  this.signedXml = ""
-  this.signatureXml = ""
-  this.signatureNode = null
-  this.signatureValue = ""
-  this.originalXmlWithIds = ""
-  this.validationErrors = []
-  this.keyInfo = null
-  this.idAttributes = [ 'Id', 'ID', 'id' ];
-         
-         
-         */
-
-
         public dynamic Options { get; set; }
         public string IdMode { get; set; }
 
-        public IEnumerable<string> References { get; } = new List<string>();
+        public IList<Reference> References { get; } = new List<Reference>();
 
         public string Id { get; set; }
 
@@ -49,23 +25,34 @@ namespace ConsoleApp
 
         public string SignatureValue { get; set; }
 
-
         public string OriginalXmlWithIds { get; set; }
 
         public object KeyInfo { get; set; }
 
         public IEnumerable<string> IdAttributes => new List<string> { "Id", "ID", "id" };
 
+        public void AddReference(string xpath, IEnumerable<string> transforms = null, string digestAlgorithm = "", string uri = "", string digestValue = "", string inclusiveNamespacesPrefixList = "", bool isEmptyUri = true)
+        {
+            var reference = new Reference
+            {
+                Xpath = xpath,
+                DigestAlgorithm = !string.IsNullOrEmpty(digestAlgorithm) ? digestAlgorithm : "http://www.w3.org/2000/09/xmldsig#sha1",
+                Uri = uri,
+                DigestValue = digestValue,
+                InclusiveNamespacesPrefixList = inclusiveNamespacesPrefixList,
+                IsEmptyUri = isEmptyUri
+            };
 
+            if (transforms != null)
+            {
+                reference.Transforms.AddRange(transforms);
+            }
+            else
+            {
+                reference.Transforms.Add("http://www.w3.org/2001/10/xml-exc-c14n#");
+            }
 
-
-
-
-
-
-
-
-
-
+            References.Add(reference);
+        }
     }
 }
